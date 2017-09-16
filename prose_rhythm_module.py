09f381d6ef4d9ef46987e2fffbba99e1cd5e4f94
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-09-15 19:26:02 -0500

    added more stuff

--- prose_rhythm_module.py
+++ prose_rhythm_module.py
@@ -9,9 +9,10 @@ from cltk.stem.latin.syllabifier import Syllabifier
 # TODO: Check if syllabifier works
 
 class prose_rhythm_module(object):
-    def __init__(self, elision, sests, punctuation, text):
+    def __init__(self, elision, sests, mute_plus_liquid, punctuation, text):
         self.elision = elision
         self.sests = sests
+        self.mute_plus_liquid  = mute_plus_liquid
         self.punctuation = punctuation
         self.text = text
 
@@ -30,11 +31,11 @@ class prose_rhythm_module(object):
         syllabified = []
         for sentence in tokenized_cola:
             syllabified_words = [syllabifier.syllabify(word) for word in sentence.split(' ')]
-            syllabified.append(syllabified_words)
+            syllabified.append(syllabified_words if syllabified_words not [])
         return syllabified
 
 if __name__ == "__main__":
     test_text = "Quo tandem usque, O Catilina; abutere nostra patientia."
-    test = prose_rhythm_module(elision=True, sests=True, punctuation=[',', ';', '.'], text=test_text)
+    test = prose_rhythm_module(elision=True, sests=True, mute_plus_liquid=True, punctuation=[',', ';', '.'], text=test_text)
     preprocessed = test.preprocessed()
     print(preprocessed)
