25ad6e5576f3ed2cc8dad4d379899def849d5e82
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-09-22 17:20:49 -0500

    change project dir, added unit test, refactored module, added long by nature

--- prose_rhythm/prose_rhythm.py
+++ prose_rhythm/prose_rhythm.py
@@ -9,7 +9,7 @@ from cltk.stem.latin.syllabifier import Syllabifier
 # TODO: Check if syllabifier works
 # TODO: Combine 'qu' syllables for first sentence in sentence
 
-class prose_rhythm_module(object):
+class prose_rhythm(object):
 
     SESTS = ['sc', 'sm', 'sp', 'st', 'z']
     MUTES = ['b', 'c', 'k', 'd', 'g', 'p', 't']
@@ -18,6 +18,8 @@ class prose_rhythm_module(object):
     VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
     SINGLE_CONSONANTS = ['b', 'c', 'd', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v']
     DOUBLE_CONSONANTS = ['x', 'z']
+    LONG_VOWELS = ['ā', 'ē', 'ī', 'ō', 'ū']
+    DIPHTHONGS = ['ae', 'au', 'ei', 'eu', 'oe', 'ui']
 
     def __init__(self, elision, sests, mute_plus_liquid, punctuation, text):
         self.elision = elision
@@ -26,19 +28,10 @@ class prose_rhythm_module(object):
         self.punctuation = punctuation
         self.text = text
 
-    @staticmethod
-    def split(txt, seps):
-        default_sep = seps[0]
-
-        # we skip seps[0] because that's the default seperator
-        for sep in seps[1:]:
-            txt = txt.replace(sep, default_sep)
-        return [i.strip() for i in txt.split(default_sep)]
-
     def preprocessed_text(self):
         """
         Tokenize text on supplied characters.
-        :return: sentence strings
+        :return: tokenized text
         :rtype : list
         """
         default_seperator = self.punctuation[0]
@@ -46,18 +39,37 @@ class prose_rhythm_module(object):
             self.text = self.text.replace(punc, default_seperator)
         return [sentence.strip() for sentence in self.text.split(default_seperator) if sentence.strip() is not '']
 
-    def preprocessed(self):
-        tokenized_cola = self.split(self.text, self.punctuation)
+    def syllabified(self):
+        """
+        Syllabify text.
+        :return: syllabified text
+        :rtype : list
+        """
+        preprocessed_text = self.preprocessed_text()
         syllabifier = Syllabifier()
         syllabified_sentence = []
-        for sentence in tokenized_cola:
-            syllabified_words = [syllabifier.syllabify(word) for word in sentence.lower().split(' ')]
+        for sentence in preprocessed_text:
+            syllabified_words = [syllabifier.syllabify(word) for word in sentence.lower().split(' ') if '[' not in word]
             syllabified_sentence.append(syllabified_words)
         syllabified = [sentence for sentence in syllabified_sentence if [] not in sentence]
         return syllabified
 
+    def long_by_nature(self, syllable):
+        """
+        Check if syllable is long by nature.
+        :return: true if long by nature
+        :rtype : boolean
+        """
+        for vowel in self.LONG_VOWELS:
+            if vowel in syllable:
+                return True
+        for diphthong in self.DIPHTHONGS:
+            if diphthong in syllable:
+                return True
+        return False
+
 if __name__ == "__main__":
     test_text = "[1] [I] Quonam meo fato, patres conscripti, fieri dicam, ut nemo his annis viginti rei publicae fuerit hostis, qui non bellum eodem tempore mihi quoque indixerit? Nec vero necesse est quemquam a me nominari; vobiscum ipsi recordamini. Mihi poenarum illi plus, quam optaram, dederunt: te miror, Antoni, quorum facta imitere, eorum exitus non perhorrescere. Atque hoc in aliis minus mirabar. Nemo enim illorum inimicus mihi fuit voluntarius, omnes a me rei publicae causa lacessiti. Tu ne verbo quidem violatus, ut audacior quam Catilina, furiosior quam Clodius viderere, ultro me maledictis lacessisti, tuamque a me alienationem commendationem tibi ad impios civis fore putavisti."
-    test = prose_rhythm_module(elision=True, sests=True, mute_plus_liquid=True, punctuation=[':',';','.','?','!'], text=test_text)
-    preprocessed = test.preprocessed_text()
+    test = prose_rhythm(elision=True, sests=True, mute_plus_liquid=True, punctuation=[':',';','.','?','!'], text=test_text)
+    preprocessed = test.syllabified()
     print(preprocessed)
