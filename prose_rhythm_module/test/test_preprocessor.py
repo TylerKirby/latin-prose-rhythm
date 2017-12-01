33e116162798688e9b4d0e341d40d5be7ab747bc
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-11-30 19:08:50 -0600

    updated method names in unit tests

--- prose_rhythm_module/test/test_preprocessor.py
+++ prose_rhythm_module/test/test_preprocessor.py
@@ -4,98 +4,89 @@ Unit tests for methods in Preprocessor class.
 
 from prose_rhythm_module.preprocessor import Preprocessor
 
-TEST_TEXT = "[1] [I] Quonam meo fato, patres conscripti, fieri dicam, ut nemo his annis viginti rei publicae fuerit " \
+TEST_TEXT1 = "[1] [I] Quonam meo fato, patres conscripti, fieri dicam, ut nemo his annis viginti rei publicae fuerit " \
             "hostis, qui non bellum eodem tempore mihi quoque indixerit? Nec vero necesse est quemquam a me nominari; " \
             "vobiscum ipsi recordamini. Mihi poenarum illi plus, quam optaram, dederunt: te miror, Antoni, quorum " \
             "facta imitere, eorum exitus non perhorrescere. Atque hoc in aliis minus mirabar. Nemo enim illorum " \
             "inimicus mihi fuit voluntarius, omnes a me rei publicae causa lacessiti. Tu ne verbo quidem violatus, ut " \
             "audacior quam Catilina, furiosior quam Clodius viderere, ultro me maledictis lacessisti, tuamque a me " \
             "alienationem commendationem tibi ad impios civis fore putavisti."
+TEST_TEXT2 = "Mihi conicio iui it, quam optaram, auditu dederunt: te miror, Antoni, quorum. Iuuenum iuuo coniectus et si cetera; coniugo auctor uia uector."
 
-preprocessor = Preprocessor(text=TEST_TEXT, punctuation=[':', ';', '.', '?', '!'])
-
-
-def test_preprocessed_text():
-	correct = ['[1] [I] Quonam meo fato, patres conscripti, fieri dicam, ut nemo his annis viginti rei publicae fuerit hostis, qui non bellum eodem tempore mihi quoque indixerit', 'Nec vero necesse est quemquam a me nominari', 'vobiscum ipsi recordamini', 'Mihi poenarum illi plus, quam optaram, dederunt', 'te miror, Antoni, quorum facta imitere, eorum exitus non perhorrescere', 'Atque hoc in aliis minus mirabar', 'Nemo enim illorum inimicus mihi fuit voluntarius, omnes a me rei publicae causa lacessiti', 'Tu ne verbo quidem violatus, ut audacior quam Catilina, furiosior quam Clodius viderere, ultro me maledictis lacessisti, tuamque a me alienationem commendationem tibi ad impios civis fore putavisti']
-	assert preprocessor.preprocessed_text() == correct
-
-def test_syllabified():
-	correct = [[['quo', 'nam'], ['me', 'o'], ['fa', 'to,'], ['pa', 'tres'], ['con', 'scrip', 'ti,'], ['fi', 'e', 'ri'], ['di', 'cam,'], ['ut'], ['ne', 'mo'], ['his'], ['an', 'nis'], ['vi', 'gin', 'ti'], ['rei'], ['pu', 'bli', 'cae'], ['fu', 'e', 'rit'], ['hos', 'tis,'], ['qui'], ['non'], ['bel', 'lum'], ['e', 'o', 'dem'], ['tem', 'po', 're'], ['mi', 'hi'], ['quo', 'que'], ['in', 'di', 'xe', 'rit']], [['nec'], ['ve', 'ro'], ['ne', 'ces', 'se'], ['est'], ['quem', 'quam'], ['a'], ['me'], ['no', 'mi', 'na', 'ri']], [['vo', 'bis', 'cum'], ['ip', 'si'], ['re', 'cor', 'da', 'mi', 'ni']], [['mi', 'hi'], ['poe', 'na', 'rum'], ['il', 'li'], ['plus,'], ['quam'], ['op', 'ta', 'ram,'], ['de', 'de', 'runt']], [['te'], ['mi', 'ror,'], ['an', 'to', 'ni,'], ['quo', 'rum'], ['fac', 'ta'], ['i', 'mi', 'te', 're,'], ['e', 'o', 'rum'], ['ex', 'i', 'tus'], ['non'], ['per', 'hor', 'res', 'ce', 're']], [['at', 'que'], ['hoc'], ['in'], ['a', 'li', 'is'], ['mi', 'nus'], ['mi', 'ra', 'bar']], [['ne', 'mo'], ['e', 'nim'], ['il', 'lo', 'rum'], ['in', 'i', 'mi', 'cus'], ['mi', 'hi'], ['fu', 'it'], ['vo', 'lun', 'ta', 'ri', 'us,'], ['om', 'nes'], ['a'], ['me'], ['rei'], ['pu', 'bli', 'cae'], ['cau', 'sa'], ['la', 'ces', 'si', 'ti']], [['tu'], ['ne'], ['ver', 'bo'], ['qui', 'dem'], ['vi', 'o', 'la', 'tus,'], ['ut'], ['au', 'da', 'ci', 'or'], ['quam'], ['ca', 'ti', 'li', 'na,'], ['fu', 'ri', 'o', 'si', 'or'], ['quam'], ['clo', 'di', 'us'], ['vi', 'de', 're', 're,'], ['ul', 'tro'], ['me'], ['ma', 'le', 'dic', 'tis'], ['la', 'ces', 'sis', 'ti,'], ['tu', 'am', 'que'], ['a'], ['me'], ['a', 'li', 'e', 'na', 'ti', 'o', 'nem'], ['com', 'men', 'da', 'ti', 'o', 'nem'], ['ti', 'bi'], ['ad'], ['im', 'pi', 'os'], ['ci', 'vis'], ['fo', 're'], ['pu', 'ta', 'vis', 'ti']]]
-	assert preprocessor.syllabified() == correct
+preprocessor1 = Preprocessor(text=TEST_TEXT1, punctuation=[':', ';', '.', '?', '!'])
+preprocessor2 = Preprocessor(text=TEST_TEXT2)
 
 def test_u_to_v():
     # word begins with 'u'
-    assert preprocessor.u_to_v("uita") == "vita"
-    assert preprocessor.u_to_v("ultor") == "ultor"
-    assert preprocessor.u_to_v("uae") == "vae"
+    assert preprocessor1._u_to_v("uita") == "vita"
+    assert preprocessor1._u_to_v("ultor") == "ultor"
+    assert preprocessor1._u_to_v("uae") == "vae"
     # word ends with 'u'
-    assert preprocessor.u_to_v("auditu") == "auditu"
-    assert preprocessor.u_to_v("arcu") == "arcu"
+    assert preprocessor1._u_to_v("auditu") == "auditu"
+    assert preprocessor1._u_to_v("arcu") == "arcu"
     # vowel + u + vowel
-    assert preprocessor.u_to_v("amaui") == "amavi"
+    assert preprocessor1._u_to_v("amaui") == "amavi"
     # consonant + u + consonant
-    assert preprocessor.u_to_v("amatus") == "amatus"
+    assert preprocessor1._u_to_v("amatus") == "amatus"
     # uu case 1
-    assert preprocessor.u_to_v("uua") == "uva"
+    assert preprocessor1._u_to_v("uua") == "uva"
     # uu case 2
-    assert preprocessor.u_to_v("uulnus") == "vulnus"
+    assert preprocessor1._u_to_v("uulnus") == "vulnus"
     # uu case 3
-    assert preprocessor.u_to_v("uult") == "vult"
+    assert preprocessor1._u_to_v("uult") == "vult"
     # vowel + u + consontant
-    assert preprocessor.u_to_v("auctor") == "auctor"
+    assert preprocessor1._u_to_v("auctor") == "auctor"
     # consonant + u + vowel
-    assert preprocessor.u_to_v("habui") == "habui"
-    assert preprocessor.u_to_v("habuerit") == "habuerit"
-    assert preprocessor.u_to_v("imbuantur") == "imbuantur"
+    assert preprocessor1._u_to_v("habui") == "habui"
+    assert preprocessor1._u_to_v("habuerit") == "habuerit"
+    assert preprocessor1._u_to_v("imbuantur") == "imbuantur"
     # consonant + u + u + consonant
-    assert preprocessor.u_to_v("tuus") == "tuus"
-    assert preprocessor.u_to_v("mortuus") == "mortuus"
-    assert preprocessor.u_to_v("perpetuum") == "perpetuum"
-    assert preprocessor.u_to_v("fluuius") == "fluvius"
-    assert preprocessor.u_to_v("exuuiae") == "exuviae"
+    assert preprocessor1._u_to_v("tuus") == "tuus"
+    assert preprocessor1._u_to_v("mortuus") == "mortuus"
+    assert preprocessor1._u_to_v("perpetuum") == "perpetuum"
+    assert preprocessor1._u_to_v("fluuius") == "fluvius"
+    assert preprocessor1._u_to_v("exuuiae") == "exuviae"
     # consonant + u + vowel (not i)
-    assert preprocessor.u_to_v("alueo") == "alveo"
+    assert preprocessor1._u_to_v("alueo") == "alveo"
     # consonant + u + vowel (i)
-    assert preprocessor.u_to_v("inuitat") == "invitat"
+    assert preprocessor1._u_to_v("inuitat") == "invitat"
     # consonantal i at start of word
-    assert preprocessor.u_to_v("iuuenum") == "iuvenum"
+    assert preprocessor1._u_to_v("iuuenum") == "iuvenum"
     # extras
-    assert preprocessor.u_to_v("adiuuo") == "adiuvo"
-    assert preprocessor.u_to_v("iui") == "ivi"
+    assert preprocessor1._u_to_v("adiuuo") == "adiuvo"
+    assert preprocessor1._u_to_v("iui") == "ivi"
 
 def test_i_to_j():
     # initial i + consonant (not u/v)
-    assert preprocessor.i_to_j("incoctus") == "incoctus"
-    assert preprocessor.i_to_j("ignis") == "ignis"
-    assert preprocessor.i_to_j("it") == "it"
+    assert preprocessor1._i_to_j("incoctus") == "incoctus"
+    assert preprocessor1._i_to_j("ignis") == "ignis"
+    assert preprocessor1._i_to_j("it") == "it"
     # initial i + vowel (not u/v)
-    assert preprocessor.i_to_j("iaceo") == "jaceo"
-    assert preprocessor.i_to_j("iecur") == "jecur"
-    assert preprocessor.i_to_j("iocus") == "jocus"
+    assert preprocessor1._i_to_j("iaceo") == "jaceo"
+    assert preprocessor1._i_to_j("iecur") == "jecur"
+    assert preprocessor1._i_to_j("iocus") == "jocus"
     # initial i + u/v
-    assert preprocessor.i_to_j("iubeo") == "jubeo"
-    assert preprocessor.i_to_j("ius") == "jus"
-    assert preprocessor.i_to_j("ivi") == "ivi"
+    assert preprocessor1._i_to_j("iubeo") == "jubeo"
+    assert preprocessor1._i_to_j("ius") == "jus"
+    assert preprocessor1._i_to_j("ivi") == "ivi"
     # i after prefix
-    assert preprocessor.i_to_j("adicio") == "adjicio"
-    assert preprocessor.i_to_j("adiectus") == "adjectus"
-    assert preprocessor.i_to_j("abicio") == "abjicio"
-    assert preprocessor.i_to_j("abiectus") == "abjectus"
-    assert preprocessor.i_to_j("conicio") == "conjicio"
-    assert preprocessor.i_to_j("coniectus") == "conjectus"
-    assert preprocessor.i_to_j("subicio") == "subjicio"
-    assert preprocessor.i_to_j("subiectus") == "subjectus"
-    assert preprocessor.i_to_j("adiudico") == "adjudico"
-    assert preprocessor.i_to_j("coniuro") == "conjuro"
-    assert preprocessor.i_to_j("coniungo") == "conjungo"
-    assert preprocessor.i_to_j("coniunctus") == "conjunctus"
-    assert preprocessor.i_to_j("adiuvo") == "adjuvo"
+    assert preprocessor1._i_to_j("adicio") == "adjicio"
+    assert preprocessor1._i_to_j("adiectus") == "adjectus"
+    assert preprocessor1._i_to_j("abicio") == "abjicio"
+    assert preprocessor1._i_to_j("abiectus") == "abjectus"
+    assert preprocessor1._i_to_j("conicio") == "conjicio"
+    assert preprocessor1._i_to_j("coniectus") == "conjectus"
+    assert preprocessor1._i_to_j("subicio") == "subjicio"
+    assert preprocessor1._i_to_j("subiectus") == "subjectus"
+    assert preprocessor1._i_to_j("adiudico") == "adjudico"
+    assert preprocessor1._i_to_j("coniuro") == "conjuro"
+    assert preprocessor1._i_to_j("coniungo") == "conjungo"
+    assert preprocessor1._i_to_j("coniunctus") == "conjunctus"
+    assert preprocessor1._i_to_j("adiuvo") == "adjuvo"
 
 def test_i_u_to_j_v():
-   TEST_TEXT1 = "Mihi conicio iui it, quam optaram, auditu dederunt: te miror, Antoni, quorum. Iuuenum iuuo coniectus et si cetera; coniugo auctor uia uector."
    CORRECT1 = "mihi conjicio ivi it, quam optaram, auditu dederunt: te miror, antoni, quorum. juvenum juvo conjectus et si cetera; conjugo auctor via vector."
-   test_class1 = Preprocessor(text=TEST_TEXT1)
-   assert test_class1.i_u_to_j_v() == CORRECT1
+   assert preprocessor2._i_u_to_j_v() == CORRECT1
 
 
 
