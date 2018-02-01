f2283cf2ebe882bc6c70567c65ae6dd142dd5baa
Tyler Kirby <tyler.kirby9398@gmail.com>
2018-01-31 19:52:17 -0600

    updated caesar

--- prose_rhythm/preprocessor.py
+++ prose_rhythm/preprocessor.py
@@ -5,7 +5,8 @@ Process normalized Latin text into an annotated data set of prosimetric informat
 
 import regex as re
 
-from cltk.stem.latin.syllabifier import Syllabifier
+from cltk.prosody.latin.Syllabifier import Syllabifier
+from prose_rhythm.normalizer import Normalizer
 
 class Preprocessor(object):
 
@@ -147,20 +148,9 @@ class Preprocessor(object):
         'Puella bona est. Puer malus est.' -> [ [{word: puella, syllables: [...], index: 0}, ... ], ... ]
         :return:list
         '''
-        # tokenize text on supplied punc
-        default_seperator = '.'
-
-        for punc in self.punctuation:
-            self.text = self.text.replace(punc, default_seperator)
-
-        for abbrev in self.ABBREV:
-            self.text = self.text.replace(abbrev, 'ABBREV')
-
-        lower_text = self.text.lower()
-        clean_text = re.sub(r'[^a-z.\sāēīōū]', '', lower_text)
-        clean_text = re.sub(r'\s{2}', ' ', clean_text)
-
-        tokenized_sentences = [sentence.strip() for sentence in clean_text.split(default_seperator) if sentence.strip() is not '']
+        normalized_text = Normalizer(self.text).normalize()
+        default_punc = "."
+        tokenized_sentences = [sentence.strip() for sentence in normalized_text.split(default_punc) if sentence.strip() is not '']
 
         tokenized_text = []
         for sentence in tokenized_sentences:
@@ -175,6 +165,17 @@ class Preprocessor(object):
 
 
 if __name__ == '__main__':
-    test_text = 'Galliā est omnīs dīvīsa in partēs trēs quārum ūnam incolunt Belgae aliam Aquītānī tertiam quī ipsōrum lingua Celtae nostra Gallī appellantur. hī omnēs linguā īnstitūtīs lēgibus inter sē differunt. Gallōs ab Aquītānīs Garunna flūmen ā Belgīs Matrona et Sēquana dīvidit. hōrum omnium fortissimī sunt Belgae proptereā quod ā cultū atque hūmānitāte prōvinciae longissimē absunt minimēque ad eōs mercātōrēs saepe commeant atque ea quae ad effēminandōs animōs pertinent important proximīque sunt Germānīs quī trāns Rhēnum incolunt quibuscum continenter bellum gerunt. quā dē causā Helvētiī quoque reliquōs Gallōs virtūte praecēdunt quod ferē cotīdiānīs proeliīs cum Germānīs contendunt cum aut suīs fīnibus eōs prohibent aut ipsī in eōrum fīnibus bellum gerunt. eōrum ūna pars quam Gallōs obtinēre dictum est initium capit ā flūmine Rhodanō continētur Garunna flūmine Ōceanō fīnibus Belgārum attingit etiam ab Sēquanīs et Helvētiīs flūmen Rhēnum vergit ad  septentriōnēs. Belgae ab extrēmīs Galliae fīnibus oriuntur pertinent ad īnferiōrem partem flūminis Rhēnī spectant in septentriōnem et orientem sōlem. Aquītānia ā Garunnā flūmine ad Pȳrēnaeōs montēs et eam partem Ōceanī quae est ad Hispāniam pertinet spectat inter occāsum sōlis et septentriōnēs. Apud Helvētiōs longē nōbilissimus fuit et dītissimus Orgetorix. is M. Messāla et P. M. Pīsōne cōnsulibus rēgnī cupiditāte inductus conjūrātiōnem nōbilitātis fēcit et cīvitātī persuāsit ut dē fīnibus suīs cum omnibus cōpiīs exīrent perfacile esse cum virtūte omnibus praestārent tōtīus Galliae imperiō potīrī. id hoc facilius iīs persuāsit quod undique locī nātūra Helvētiī continentur ūna ex parte flūmine Rhēnō lātissimō atque altissimō quī agrum Helvētium ā Germānīs dīvidit altera ex parte monte Jūrā altissimō quī est inter Sēquanōs et Helvētiōs tertia lacū Lemannō et flūmine Rhodanō quī prōvinciam nostram ab Helvētiīs dīvidit. hīs rēbus fīēbat ut et minus lātē vagārentur et minus facile fīnitimīs bellum īnferre possent quā ex parte hominēs bellandī cupidī magnō dolōre adficiēbantur. prō multitūdine autem hominum et prō glōriā bellī atque fortitūdinis angustōs sē fīnēs habēre arbitrābantur quī in longitūdinem mīlia passuum ccxl in lātitūdinem clxxx patēbant. Hīs rēbus adductī et auctōritāte Orgetorigis permōtī cōnstituērunt ea quae ad proficīscendum pertinērent comparāre jūmentōrum et carrōrum quam maximum numerum coemere sēmentēs quam maximās facere ut in itinere cōpia frūmentī suppeteret cum proximīs cīvitātibus pācem et amīcitiam cōnfirmāre. ad eās rēs cōnficiendās biennium sibi satis esse dūxērunt in  tertium annum profectiōnem lēge cōnfirmant. ad eās rēs cōnficiendās Orgetorix dēligitur. is sibi lēgātiōnem ad cīvitātēs suscēpit. in eō itinere persuādet Castīcō Catamantaloedis fīliō Sēquanō cūjus pater rēgnum in Sēquanīs multōs annōs obtinuerat et ab senātū populī Rōmānī amīcus appellātus erat ut rēgnum in cīvitāte suā occupāret quod pater ante habuerat itemque Dumnorigī Haeduō frātrī Dīviciācī quī eō tempore prīncipātum in cīvitāte obtinēbat ac maximē plēbī acceptus erat ut īdem cōnārētur persuādet eīque fīliam suam in mātrimōnium dat. perfacile factū esse illīs probat cōnāta perficere proptereā quod ipse suae cīvitātis imperium obtentūrus esset nōn esse dubium quīn tōtīus Galliae plūrimum Helvētiī possent sē suīs cōpiīs suōque exercitū illīs rēgna conciliātūrum cōnfirmat. hāc ōrātiōne adductī inter sē fidem et jūs jūrandum dant et rēgnō occupātō per trēs potentissimōs ac firmissimōs populōs tōtīus Galliae sēsē potīrī posse spērant. Ea rēs est Helvētiīs per indicium ēnūntiāta. mōribus suīs Orgetorigem ex vinculīs causam dīcere coēgērunt damnātum poenam sequī oportēbat ut ignī cremārētur. diē cōnstitūtā causae dictiōnis Orgetorix ad jūdicium omnem suam familiam ad hominum mīlia decem undique coēgit et omnēs clientēs obaerātōsque suōs quōrum magnum numerum habēbat eōdem condūxit per eōs nē causam dīceret sē ēripuit. cum cīvitās ob eam rem incitātā armīs jūs suum exsequī cōnārētur multitūdinemque hominum ex agrīs magistrātūs cōgerent Orgetorix mortuus est neque abest suspīciō ut Helvētiī arbitrantur quīn ipse sibi mortem cōnscīverit. Post ējus mortem nihilōminus Helvētiī id quod cōnstituerant facere cōnantur ut ē fīnibus suīs exeant. ubi  jam sē ad eam rem parātōs esse arbitrātī sunt oppida sua omnia numerō ad duodecim vīcōs ad quadringentōs reliqua prīvāta aedificia incendunt frūmentum omne praeter quod sēcum portātūrī erant combūrunt ut domum reditiōnis spē sublāta parātiōrēs ad omnia perīcula subeunda essent trium mēnsum molita cibāria sibi quemque domō efferre jubent. persuādent Rauracīs et Tulingīs et Latobrigīs fīnitimīs utī eōdem ūsī cōnsiliō oppidīs suīs vīcīsque exustīs ūna cum iīs proficīscantur Boiōsque quī trāns Rhēnum incoluerant et in agrum Nōricum trānsierant Norēiamque oppugnābant receptōs ad sē sociōs sibi adscīscunt.'
-    test_class = Preprocessor(test_text, ['.', ';'], 'test')
-    print(test_class.tokenize())
+    test_text = "Galliā est omnīs dīvīsa in partēs trēs quārum ūnam incolunt Belgae aliam Aquītānī tertiam quī ipsōrum " \
+                "lingua Celtae nostra Gallī appellantur. hī omnēs linguā īnstitūtīs lēgibus inter sē differunt. Gallōs" \
+                " ab Aquītānīs Garunna flūmen ā Belgīs Matrona et Sēquana dīvidit. hōrum omnium fortissimī sunt Belgae " \
+                "proptereā quod ā cultū atque hūmānitāte prōvinciae longissimē absunt minimēque ad eōs mercātōrēs " \
+                "saepe commeant atque ea quae ad effēminandōs animōs pertinent important proximīque sunt Germānīs " \
+                "quī trāns Rhēnum incolunt quibuscum continenter bellum gerunt. quā dē causā Helvētiī quoque reliquōs" \
+                " Gallōs virtūte praecēdunt quod ferē cotīdiānīs proeliīs cum Germānīs contendunt cum aut suīs fīnibus" \
+                " eōs prohibent aut ipsī in eōrum fīnibus bellum gerunt. eōrum ūna pars quam Gallōs obtinēre dictum est" \
+                " initium capit ā flūmine Rhodanō continētur Garunna flūmine Ōceanō fīnibus Belgārum attingit etiam " \
+                "ab Sēquanīs et Helvētiīs flūmen Rhēnum vergit ad  septentriōnēs. Belgae ab extrēmīs Galliae" \
+                " fīnibus oriuntur pertinent ad īnferiōrem partem flūminis Rhēnī spectant in septentriōnem et" \
+                " orientem sōlem."
+    test_sentences = test_text.split('.')
+    print(Preprocessor(test_text).tokenize())
---
+++ test/test_preprocessor.py
@@ -0,0 +1,137 @@
+'''
+Unit tests for methods in Preprocessor class.
+'''
+
+from prose_rhythm.preprocessor import Preprocessor
+
+TEST_TEXT1 = '[1] [I] Quonam meo fato, patres conscripti, fieri dicam, ut nemo his annis viginti rei publicae fuerit ' \
+            'hostis, qui non bellum eodem tempore mihi quoque indixerit? Nec vero necesse est quemquam a me nominari; ' \
+            'vobiscum ipsi recordamini. Mihi poenarum illi plus, quam optaram, dederunt: te miror, Antoni, quorum ' \
+            'facta imitere, eorum exitus non perhorrescere. Atque hoc in aliis minus mirabar. Nemo enim illorum ' \
+            'inimicus mihi fuit voluntarius, omnes a me rei publicae causa lacessiti. Tu ne verbo quidem violatus, ut ' \
+            'audacior quam Catilina, furiosior quam Clodius viderere, ultro me maledictis lacessisti, tuamque a me ' \
+            'alienationem commendationem tibi ad impios civis fore putavisti.'
+TEST_TEXT2 = 'Mihi conicio iui it, quam optaram, auditu dederunt: te miror, Antoni, quorum. Iuuenum iuuo coniectus et si cetera; coniugo auctor uia uector.'
+TEST_TEXT3 = 'Mihi coniciō iui it, quam optāram, auditū dedērunt: te miror, Antōnī, quorum. Iuuēnum iuuō coniectus et si cetera; coniugo auctor uiā uector.'
+
+preprocessor1 = Preprocessor(text=TEST_TEXT1, punctuation=[':', ';', '.', '?', '!'])
+preprocessor2 = Preprocessor(text=TEST_TEXT2)
+preprocessor3 = Preprocessor(text=TEST_TEXT3)
+
+def test_tokenize_syllables():
+    test1 = preprocessor3._tokenize_syllables('mihi')
+    test2 = preprocessor3._tokenize_syllables('ivi')
+    test3 = preprocessor3._tokenize_syllables('audītū')
+    test4 = preprocessor3._tokenize_syllables('conjiciō')
+    test5 = preprocessor3._tokenize_syllables('ā')
+    test6 = preprocessor3._tokenize_syllables('appelantur')
+    # syllable and index
+    assert test1[0]['syllable'] == 'mi'
+    assert test1[0]['index'] == 0
+    assert test1[1]['syllable'] ==  'hi'
+    assert test1[1]['index'] == 1
+    assert len(test1) == 2
+    assert test2[0]['syllable'] == 'i'
+    assert test2[0]['index'] == 0
+    assert test2[1]['syllable'] == 'vi'
+    assert test2[1]['index'] == 1
+    assert len(test2) == 2
+    assert test3[0]['syllable'] == 'au'
+    assert test3[0]['index'] == 0
+    assert test3[1]['syllable'] == 'dī'
+    assert test3[1]['index'] == 1
+    assert test3[2]['syllable'] == 'tū'
+    assert test3[2]['index'] == 2
+    assert len(test3) == 3
+    assert test4[0]['syllable'] == 'con'
+    assert test4[0]['index'] == 0
+    assert test4[1]['syllable'] == 'ji'
+    assert test4[1]['index'] == 1
+    assert test4[2]['syllable'] == 'ci'
+    assert test4[2]['index'] == 2
+    assert test4[3]['syllable'] == 'ō'
+    assert test4[3]['index'] == 3
+    assert len(test4) == 4
+    assert test5[0]['syllable'] == 'ā'
+    assert test5[0]['index'] == 0
+    assert len(test5) == 1
+    # long by nature
+    assert test1[0]['long_by_nature'] == False
+    assert test1[1]['long_by_nature'] == False
+    assert test2[0]['long_by_nature'] == False
+    assert test2[1]['long_by_nature'] == False
+    assert test3[0]['long_by_nature'] == True
+    assert test3[1]['long_by_nature'] == True
+    assert test3[2]['long_by_nature'] == True
+    assert test4[0]['long_by_nature'] == False
+    assert test4[1]['long_by_nature'] == False
+    assert test4[2]['long_by_nature'] == False
+    assert test4[3]['long_by_nature'] == True
+    assert test5[0]['long_by_nature'] == True
+    # accent
+    assert test1[0]['accented'] == True
+    assert test1[1]['accented'] == False
+    assert test2[0]['accented'] == True
+    assert test2[1]['accented'] == False
+    assert test3[0]['accented'] == False
+    assert test3[1]['accented'] == True
+    assert test3[2]['accented'] == False
+    assert test4[0]['accented'] == False
+    assert test4[1]['accented'] == True
+    assert test4[2]['accented'] == False
+    assert test4[3]['accented'] == False
+    assert test5[0]['accented'] == True
+    assert test6[0]['accented'] == False
+    assert test6[1]['accented'] == False
+    assert test6[2]['accented'] == True
+    assert test6[3]['accented'] == False
+    # long by position
+    assert test1[0]['long_by_position'] == (False, None)
+    assert test1[1]['long_by_position'] == (False, None)
+    assert test2[0]['long_by_position'] == (False, None)
+    assert test2[1]['long_by_position'] == (False, None)
+    assert test3[0]['long_by_position'] == (False, None)
+    assert test3[1]['long_by_position'] == (False, None)
+    assert test3[2]['long_by_position'] == (False, None)
+    assert test4[0]['long_by_position'] == (True, None)
+    assert test4[1]['long_by_position'] == (False, None)
+    assert test4[2]['long_by_position'] == (False, None)
+    assert test4[3]['long_by_position'] == (False, None)
+    assert test5[0]['long_by_position'] == (False, None)
+    # mute plus liquid
+    test6 = preprocessor3._tokenize_syllables('volūcris')
+    assert test6[1]['long_by_position'] == (False, 'mute+liquid')
+    test7 = preprocessor3._tokenize_syllables('oblinō')
+    assert test7[0]['long_by_position'] == (True, None)
+
+def test_tokenize_words():
+    test1 = preprocessor3._tokenize_words('mihi conjiciō ivi it quam optāram auditū dedērunt te miror antōnī quorum.')
+    test2 = preprocessor3._tokenize_words('Galliaque humanitate.')
+    # word and index
+    assert test1[0]['word'] == 'mihi'
+    assert test1[0]['index'] == 0
+    assert test1[1]['word'] == 'conjiciō'
+    assert test1[1]['index'] == 1
+    assert len(test1) == 12
+    # syllables and syllable count
+    assert test1[0]['syllables'] == preprocessor3._tokenize_syllables('mihi')
+    assert test1[1]['syllables_count'] == 4
+    assert test1[3]['syllables_count'] == 1
+    # elision
+    assert test1[0]['syllables'][-1]['elide'] == (False, None)
+    assert test1[3]['syllables'][-1]['elide'] == (False, None)
+    assert test1[1]['syllables'][-1]['elide'] == (True, 'strong')
+    assert test1[2]['syllables'][-1]['elide'] == (True, 'weak')
+    assert test1[4]['syllables'][-1]['elide'] == (True, 'strong')
+    assert test2[0]['syllables'][-1]['elide'] ==(True, 'weak')
+    # long by position inter word
+    test2 = preprocessor3._tokenize_words('puella est bona it con.')
+    assert test2[3]['syllables'][-1]['long_by_position'] == (True, None)
+    # sests test
+    test3 = preprocessor3._tokenize_words('a spes co i no xe cta.')
+    assert test3[0]['syllables'][0]['long_by_position'] == (False, 'sest')
+    assert test3[1]['syllables'][0]['long_by_position'] == (True, None)
+    assert test3[2]['syllables'][0]['long_by_position'] == (False, None)
+    assert test3[3]['syllables'][0]['long_by_position'] == (False, None)
+    assert test3[4]['syllables'][0]['long_by_position'] == (True, None)
+    assert test3[5]['syllables'][0]['long_by_position'] == (True, None)
