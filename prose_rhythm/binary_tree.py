607b07d0fdb8b552823ce1bbbe01f5e31f5e6b8c
Tyler Kirby <tyler.kirby9398@gmail.com>
2018-04-16 20:49:24 -0500

    added probability calculation

--- prose_rhythm/binary_tree.py
+++ prose_rhythm/binary_tree.py
@@ -3,25 +3,37 @@ Classes for constructing binary tree from rhythm data.
 """
 
 import json
+import uuid
 
 
 class Node:
     """
     Structure rhythm data as a single node for a single rhythm.
     """
-    def __init__(self, rhythm):
-        self.short = None
-        self.long = None
+    def __init__(self, rhythm, parent_id, short=None, long=None, frequency=1, probability=0, parent_frequency=0):
+        self.short = short
+        self.long = long
         self.rhythm = rhythm
-        self.frequency = 1
-        self.probability = 0
+        self.frequency = frequency
+        self.probability = probability
+        self.id = str(uuid.uuid4())
+        self.parent_id = parent_id
 
     def __repr__(self):
         """
         Represent node as a string dictionary.
         :return: string representation of node
         """
-        return str({"rhythm": self.rhythm, "frequency": self.frequency, "short": self.short, "long": self.long})
+        return str({"rhythm": self.rhythm, "frequency": self.frequency,
+                    "probability": self.probability, "id": self.id,
+                    "parent_id": self.parent_id})
+
+    def __iter__(self):
+        if self.short:
+            yield from self.short
+        yield self
+        if self.long:
+            yield from self.long
 
     def to_json(self):
         """
@@ -35,8 +47,7 @@ class Tree:
     Constuct binary tree from rhythm data.
     """
     def __init__(self):
-        self.root = Node("x")
-        self.root.frequency -= 1
+        self.root = Node("x", frequency=0, parent_id=None)
 
     def __repr__(self):
         """
@@ -70,17 +81,26 @@ class Tree:
                         if curr_node.long is not None:
                             curr_node.long.frequency += 1
                         else:
-                            curr_node.long = Node("-")
+                            curr_node.long = Node("-", parent_id=curr_node.id)
                     elif char == "u":
                         if curr_node.short is not None:
                             curr_node.short.frequency += 1
                         else:
-                            curr_node.short = Node("u")
+                            curr_node.short = Node("u", parent_id=curr_node.id)
                 else:
                     if char == "-":
                         curr_node = curr_node.long
                     if char == "u":
                         curr_node = curr_node.short
+
+    def calculate_probability(self):
+        nodes = [node.__dict__ for node in self.root if node.rhythm != "x"]
+        for node in self.root:
+            node = node.__dict__
+            parent_node = [n for n in nodes if n["id"] == node["parent_id"]]
+            if len(parent_node) == 1:
+                node["probability"] = node["frequency"] / parent_node[0]["frequency"]
+
 
 if __name__ == "__main__":
     test_rhythms1 = ["-u-x", "--ux", "uu-x"]
@@ -88,4 +108,5 @@ if __name__ == "__main__":
     tree.add(test_rhythms1[0])
     tree.add(test_rhythms1[1])
     tree.add(test_rhythms1[2])
+    tree.calculate_probability()
     print(tree.to_json())
